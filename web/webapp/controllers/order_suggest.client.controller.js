5bd83fa0b1aa9bd0bd82a89d64e8e73829796211
yujia123 <1963968619@qq.com>
2016-02-16 14:25:24 +0800

    Add suggest left style

--- web/webapp/controllers/order_suggest.client.controller.js
+++ web/webapp/controllers/order_suggest.client.controller.js
@@ -1,6 +1,6 @@
 /**
  * Created by zenghong on 16/1/15.
  */
-angular.module('agilesales-web').controller('OrderSuggestCtrl',['$scope','$state', function ($scope,$state) {
-  $state.go('order_suggest.suggest_home');
+angular.module('agilesales-web').controller('OrderSuggestCtrl', ['$scope', '$state', function ($scope, $state) {
+  $scope.location = window.location;
 }]);

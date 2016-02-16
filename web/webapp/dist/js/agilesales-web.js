b023560ea5c0b2bf0993fc1ed341114d4fcd2422
yujia123 <1963968619@qq.com>
2016-02-16 18:34:39 +0800

    Update header texts

--- web/webapp/dist/js/agilesales-web.js
+++ web/webapp/dist/js/agilesales-web.js
@@ -888,8 +888,28 @@ angular.module('agilesales-web').controller('HomeCtrl', function () {
 /**
  * Created by zenghong on 16/1/15.
  */
-angular.module('agilesales-web').controller('IndexCtrl', ['$scope',function ($scope) {
+angular.module('agilesales-web').controller('IndexCtrl', ['$scope', function ($scope) {
   $scope.location = window.location;
+  $scope.headers = [
+    {
+      text: '建议订单'
+    },
+    {
+      text: '查看订单'
+    },
+    {
+      text: '历史数据'
+    },
+    {
+      text: '系统设置'
+    },
+    {
+      text: '订单详情'
+    },
+    {
+      text: '查看报表'
+    }
+  ]
 }]);
 /**
  * Created by zenghong on 16/1/15.

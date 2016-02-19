e7c7202900e8b71932de76fae20db8b465046510
yujia123 <1963968619@qq.com>
2016-02-19 17:17:53 +0800

    Update client style

--- web/webapp/controllers/order_history.client.controller.js
+++ web/webapp/controllers/order_history.client.controller.js
@@ -1,11 +1,17 @@
 /**
  * Created by zenghong on 16/1/15.
  */
-angular.module('agilesales-web').controller('OrderHistoryCtrl', ['$scope', function ($scope) {
+angular.module('agilesales-web').controller('OrderHistoryCtrl', ['$scope', 'AuthService', function ($scope, AuthService) {
   $scope.importBtns = [];
   $scope.location = window.location;
-  $scope.$on('suggest.import.changed', function (event,data) {
+  $scope.$on('suggest.import.changed', function (event, data) {
     $scope.importBtns = data.btns;
     $scope.title = data.title;
   });
+
+  $scope.user = AuthService.getUser() || {};
+  AuthService.onUserUpdated('AuthService', function (user) {
+    $scope.user = user;
+  });
+
 }]);

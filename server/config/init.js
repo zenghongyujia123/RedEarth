6ef296c85e19d56415e2867cf83527d2d5f03f7e
yujia123 <1963968619@qq.com>
2016-02-25 20:04:09 +0800

    Update config

--- server/config/init.js
+++ server/config/init.js
@@ -14,7 +14,7 @@ module.exports = function() {
      * Before we begin, lets set the environment variable
      * We'll Look for a valid NODE_ENV variable and if one cannot be found load the development NODE_ENV
      */
-    glob('./server/config/env/' + process.env.NODE_ENV + '.js', {
+    glob('./config/env/' + process.env.NODE_ENV + '.js', {
         sync: true
     }, function(err, environmentFiles) {
         console.log();

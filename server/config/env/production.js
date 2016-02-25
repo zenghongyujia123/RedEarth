5e63f1c2b909fa6367562f3d89ce18f3a315017e
yujia123 <1963968619@qq.com>
2016-02-25 19:34:24 +0800

    Update password

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    appDb: 'mongodb://hardy:Social2014@10.13.46.65:27017/redearth-pro',
+    appDb: 'mongodb://hardy:Social+_30@10.13.46.65:27017/redearth-pro',
     loggerLevel: 'debug',
     app: {
         title: 'zhuzhuqs - Development Environment'

4a11fb1cc0b3090ae7ad9c9db527a149f7df541a
yujia123 <1963968619@qq.com>
2016-03-04 17:45:00 +0800

    Update table style for d2

--- web/webapp/dist/js/agilesales-web.js
+++ web/webapp/dist/js/agilesales-web.js
@@ -268,6 +268,18 @@ angular.module('agilesales-web').config(['$stateProvider', '$urlRouterProvider',
       });
 
     }]);
+angular.module('agilesales-web').directive('zzFloatLeft', ['$rootScope', function ($rootScope) {
+  return {
+    restrict: 'A',
+    replace: false,
+    controller: function ($scope, $element) {
+      var parent = $element.parents('.re-table');
+      parent.scroll(function () {
+        $element.css({left: parent.scrollLeft() + 'px'});
+      });
+    }
+  }
+}]);
 /**
  * Created by zenghong on 16/1/19.
  */

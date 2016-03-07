38099a7e2f5b1c0d7dd2555cb8d7da8653df061b
yujia123 <1963968619@qq.com>
2016-03-07 19:30:50 +0800

    Update area suggest table

--- web/webapp/controllers/suggest_area_suggest_result.client.controller.js
+++ web/webapp/controllers/suggest_area_suggest_result.client.controller.js
@@ -21,7 +21,7 @@ angular.module('agilesales-web').controller('SuggestAreaSuggestResultCtrl', ['$s
 
           if (data.status !== '已审核') {
             $scope.$emit('suggest.import.changed', {
-              title: '建议订单 地区建议订单（SKU）=当月预测-[地区库存(包括店柜库存) +在途-未来6月销售预测-其他订单(批发)-安全库存）] * 产品分类（ABC）?%',
+              title: '建议订单 地区建议订单（SKU）=当月预测-[地区库存(包括店柜库存) +在途-未来6月销售预测-其他订单(批发)-安全库存）]',
               btns: [
                 {
                   text: '提交',

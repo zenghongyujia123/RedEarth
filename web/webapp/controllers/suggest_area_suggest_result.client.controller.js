488d7a16bc0d56e41faa0689151f789800b39bf5
yujia123 <1963968619@qq.com>
2016-03-07 15:43:12 +0800

    Remove test code

--- web/webapp/controllers/suggest_area_suggest_result.client.controller.js
+++ web/webapp/controllers/suggest_area_suggest_result.client.controller.js
@@ -63,9 +63,6 @@ angular.module('agilesales-web').controller('SuggestAreaSuggestResultCtrl', ['$s
         sale.next_month_sales_forecast_4 +
         sale.next_month_sales_forecast_5;
       var other_order_count = parseInt(sale.D02) + parseInt(sale.D03) + parseInt(sale.D04);
-      if (sale.product_number === 'ACC13561') {
-        var a = parseInt(sale.next_month_sales_forecast_0 - (sale.last_month_stock_count + sale.last_month_onway_count - next_6_month_forecast - other_order_count - sale.safe_stock));
-      }
       sale.system_suggest_count = parseInt(sale.next_month_sales_forecast_0 - (sale.last_month_stock_count + sale.last_month_onway_count - next_6_month_forecast - other_order_count - sale.safe_stock));
 
       if (sale.system_suggest_count_modify === 0) {

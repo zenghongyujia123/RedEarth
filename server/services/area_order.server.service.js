a58f9d90d28e0290d5e0572c61e811f703750cf2
yujia123 <1963968619@qq.com>
2016-03-09 22:48:32 +0800

    Update for test

--- server/services/area_order.server.service.js
+++ server/services/area_order.server.service.js
@@ -474,7 +474,7 @@ exports.approveAreaOrder = function (user, order, callback) {
       return callback({err: error.business.area_sale_not_found});
     }
 
-    areaSales.D01_approve = order.D01_approve;
+    areaSales.D01_approve = parseInt(order.D01_approve) === 0 ? 0 : parseInt(order.D01_approve);
     areaSales.D02_approve = order.D02_approve;
     areaSales.D03_approve = order.D03_approve;
     areaSales.D04_approve = order.D04_approve;

9a7428d044b092b8cb16261ea40d4aa1363f2119
zenghong <zenghong@zenghongdeMacBook-Pro.local>
2016-06-06 18:20:50 +0800

    Add order detail export logic

--- web/webapp/controllers/order_detail.client.controller.js
+++ web/webapp/controllers/order_detail.client.controller.js
@@ -1,10 +1,14 @@
 /**
  * Created by zenghong on 16/1/15.
  */
-angular.module('agilesales-web').controller('OrderDetailCtrl', ['$scope', '$stateParams', '$state', 'AreaOrderService', 'AuthService','Loading',
-  function ($scope, $stateParams, $state, AreaOrderService, AuthService,Loading) {
+angular.module('agilesales-web').controller('OrderDetailCtrl', ['$scope', '$stateParams', '$state', 'AreaOrderService', 'AuthService', 'Loading', 'ExcelReaderService',
+  function ($scope, $stateParams, $state, AreaOrderService, AuthService, Loading, ExcelReaderService) {
     $scope.order_number = $stateParams.order_number;
     $scope.orders = [];
+    $scope.importBtns = [{
+      text: '导出',
+      clickCallback: exportExecl
+    }];
     $scope.getAreaOrderDetail = function () {
       Loading.show();
       AreaOrderService.getAreaOrderDetail($scope.order_number).then(function (data) {
@@ -39,4 +43,85 @@ angular.module('agilesales-web').controller('OrderDetailCtrl', ['$scope', '$stat
         console.log(data);
       });
     };
+
+    $scope.exportExcel = function () {
+      var execlReader = ExcelReaderService.getReader();
+      var rows = [[
+        '产品名称',
+        'SKU编码',
+        '产品条码',
+        '品类',
+        '中分类名称',
+        '销售价格',
+        'ABC分类',
+        'ABC类说明',
+        'ABC类别%',
+        '上月月结库存包括店柜库存',
+        '在途已订购未交货',
+        '安全库存2个月',
+        '当月销售预测',
+        '后1月销售预测',
+        '后2月销售预测',
+        '后3月销售预测',
+        '后4月销售预测',
+        '后5月销售预测',
+        '后6月销售预测',
+        '当月上传批发订单',
+        '当月订单系统建议',
+        '修改',
+        '修改%',
+        '备注',
+        '确认超额订购?',
+        '上传订单适用装',
+        '最终审批适用装',
+        '上传订单陈列',
+        '最终审批陈列',
+        '最终审批大货数',
+        '总金额',
+        '审批状态'
+      ]];
+
+      $scope.orders.forEach(function (o) {
+        rows.push([
+          o.product.product_name,
+          o.product.product_number,
+          o.product.product_barcode,
+          o.product.category,
+          o.product.mid_classify,
+          o.product.sales_price,
+          o.product.abc_classify,
+          o.product.abc_classify_explain,
+          o.product.abc_category,
+          o.last_month_stock_count,
+          o.last_month_onway_count,
+          o.safe_stock,
+          o.next_month_sales_forecast_0,
+          o.next_month_sales_forecast_1,
+          o.next_month_sales_forecast_2,
+          o.next_month_sales_forecast_3,
+          o.next_month_sales_forecast_4,
+          o.next_month_sales_forecast_5,
+          o.next_month_sales_forecast_6,
+          o.D02,
+          o.system_suggest_count,
+          o.system_suggest_count_modify,
+          o.system_suggest_count_modify_percent,
+          o.remark,
+          '',
+          o.D03,
+          o.D03_approve,
+          o.D04,
+          o.D04_approve,
+          o.D01_approve,
+          '',
+          o.status
+        ]);
+      });
+
+      execlReader.exportExcel(rows);
+    };
+
+    function exportExecl() {
+      $scope.exportExcel();
+    }
   }]);
--- web/webapp/dist/js/agilesales-web.js
+++ web/webapp/dist/js/agilesales-web.js
@@ -380,19 +380,19 @@ angular.module('agilesales-web').directive('agDialogConfirm', ['$rootScope',func
 /**
  * Created by zenghong on 16/1/18.
  */
-angular.module('agilesales-web').directive('agDialogInput', ['$rootScope', function ($rootScope) {
+angular.module('agilesales-web').directive('agDialogSelect', ['$rootScope', function ($rootScope) {
   return {
     restrict: 'AE',
-    templateUrl: 'directives/dialog_input/dialog_input.client.view.html',
+    templateUrl: 'directives/dialog_select/dialog_select.client.view.html',
     replace: true,
     scope: {},
     link: function ($scope, $element, $attrs) {
+      $scope.options = [];
       $scope.info = {
         title: '',
         contents: [{
           key: '请输入拜访卡名称',
-          tip: '点击输入名称',
-          value: ''
+          value: '点击输入名称'
         }],
         color: 'blue'
       };
@@ -405,18 +405,35 @@ angular.module('agilesales-web').directive('agDialogInput', ['$rootScope', funct
       };
       $scope.submit = function () {
         $element.removeClass('show');
-        if ($scope.info.callback) {
-          $scope.info.callback($scope.info);
+        $scope.info.callback($scope.info);
+      };
+      $scope.toggleOptions = function (index) {
+        if ($element.find('.ag-row-option-container').eq(index).hasClass('show')) {
+          $scope.hideOptions(index);
+        }
+        else {
+          $scope.showOptions(index);
         }
       };
-      $rootScope.$on('show.dialogInput', function (event, data) {
+
+      $scope.selectOption = function (content, option) {
+        content.value = option;
+      };
+
+      $rootScope.$on('show.dialogSelect', function (event, data) {
         setTheme(data);
         $scope.show();
       });
-
       function setTheme(info) {
         $element.find('.ag-dialog-panel').removeClass($scope.info.color).addClass(info.color);
         $scope.info = info;
+      }
+
+      $scope.showOptions = function (index) {
+        $element.find('.ag-row-option-container').eq(index).addClass('show');
+      };
+      $scope.hideOptions = function (index) {
+        $element.find('.ag-row-option-container').eq(index).removeClass('show');
       }
     }
   }
@@ -488,19 +505,19 @@ angular.module('agilesales-web').directive('agDialogUpload', ['$rootScope', 'Exc
 /**
  * Created by zenghong on 16/1/18.
  */
-angular.module('agilesales-web').directive('agDialogSelect', ['$rootScope', function ($rootScope) {
+angular.module('agilesales-web').directive('agDialogInput', ['$rootScope', function ($rootScope) {
   return {
     restrict: 'AE',
-    templateUrl: 'directives/dialog_select/dialog_select.client.view.html',
+    templateUrl: 'directives/dialog_input/dialog_input.client.view.html',
     replace: true,
     scope: {},
     link: function ($scope, $element, $attrs) {
-      $scope.options = [];
       $scope.info = {
         title: '',
         contents: [{
           key: '请输入拜访卡名称',
-          value: '点击输入名称'
+          tip: '点击输入名称',
+          value: ''
         }],
         color: 'blue'
       };
@@ -513,35 +530,18 @@ angular.module('agilesales-web').directive('agDialogSelect', ['$rootScope', func
       };
       $scope.submit = function () {
         $element.removeClass('show');
-        $scope.info.callback($scope.info);
-      };
-      $scope.toggleOptions = function (index) {
-        if ($element.find('.ag-row-option-container').eq(index).hasClass('show')) {
-          $scope.hideOptions(index);
-        }
-        else {
-          $scope.showOptions(index);
+        if ($scope.info.callback) {
+          $scope.info.callback($scope.info);
         }
       };
-
-      $scope.selectOption = function (content, option) {
-        content.value = option;
-      };
-
-      $rootScope.$on('show.dialogSelect', function (event, data) {
+      $rootScope.$on('show.dialogInput', function (event, data) {
         setTheme(data);
         $scope.show();
       });
+
       function setTheme(info) {
         $element.find('.ag-dialog-panel').removeClass($scope.info.color).addClass(info.color);
         $scope.info = info;
-      }
-
-      $scope.showOptions = function (index) {
-        $element.find('.ag-row-option-container').eq(index).addClass('show');
-      };
-      $scope.hideOptions = function (index) {
-        $element.find('.ag-row-option-container').eq(index).removeClass('show');
       }
     }
   }
@@ -2062,10 +2062,14 @@ angular.module('agilesales-web').controller('IndexCtrl', ['$scope', '$rootScope'
 /**
  * Created by zenghong on 16/1/15.
  */
-angular.module('agilesales-web').controller('OrderDetailCtrl', ['$scope', '$stateParams', '$state', 'AreaOrderService', 'AuthService','Loading',
-  function ($scope, $stateParams, $state, AreaOrderService, AuthService,Loading) {
+angular.module('agilesales-web').controller('OrderDetailCtrl', ['$scope', '$stateParams', '$state', 'AreaOrderService', 'AuthService', 'Loading', 'ExcelReaderService',
+  function ($scope, $stateParams, $state, AreaOrderService, AuthService, Loading, ExcelReaderService) {
     $scope.order_number = $stateParams.order_number;
     $scope.orders = [];
+    $scope.importBtns = [{
+      text: '导出',
+      clickCallback: exportExecl
+    }];
     $scope.getAreaOrderDetail = function () {
       Loading.show();
       AreaOrderService.getAreaOrderDetail($scope.order_number).then(function (data) {
@@ -2100,6 +2104,87 @@ angular.module('agilesales-web').controller('OrderDetailCtrl', ['$scope', '$stat
         console.log(data);
       });
     };
+
+    $scope.exportExcel = function () {
+      var execlReader = ExcelReaderService.getReader();
+      var rows = [[
+        '产品名称',
+        'SKU编码',
+        '产品条码',
+        '品类',
+        '中分类名称',
+        '销售价格',
+        'ABC分类',
+        'ABC类说明',
+        'ABC类别%',
+        '上月月结库存包括店柜库存',
+        '在途已订购未交货',
+        '安全库存2个月',
+        '当月销售预测',
+        '后1月销售预测',
+        '后2月销售预测',
+        '后3月销售预测',
+        '后4月销售预测',
+        '后5月销售预测',
+        '后6月销售预测',
+        '当月上传批发订单',
+        '当月订单系统建议',
+        '修改',
+        '修改%',
+        '备注',
+        '确认超额订购?',
+        '上传订单适用装',
+        '最终审批适用装',
+        '上传订单陈列',
+        '最终审批陈列',
+        '最终审批大货数',
+        '总金额',
+        '审批状态'
+      ]];
+
+      $scope.orders.forEach(function (o) {
+        rows.push([
+          o.product.product_name,
+          o.product.product_number,
+          o.product.product_barcode,
+          o.product.category,
+          o.product.mid_classify,
+          o.product.sales_price,
+          o.product.abc_classify,
+          o.product.abc_classify_explain,
+          o.product.abc_category,
+          o.last_month_stock_count,
+          o.last_month_onway_count,
+          o.safe_stock,
+          o.next_month_sales_forecast_0,
+          o.next_month_sales_forecast_1,
+          o.next_month_sales_forecast_2,
+          o.next_month_sales_forecast_3,
+          o.next_month_sales_forecast_4,
+          o.next_month_sales_forecast_5,
+          o.next_month_sales_forecast_6,
+          o.D02,
+          o.system_suggest_count,
+          o.system_suggest_count_modify,
+          o.system_suggest_count_modify_percent,
+          o.remark,
+          '',
+          o.D03,
+          o.D03_approve,
+          o.D04,
+          o.D04_approve,
+          o.D01_approve,
+          '',
+          o.status
+        ]);
+      });
+
+      execlReader.exportExcel(rows);
+    };
+
+    function exportExecl() {
+      $scope.exportExcel();
+    }
   }]);
 /**
  * Created by zenghong on 16/1/15.
